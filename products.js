f508068353fb8fc9c341a5aa2e815133ae8b31e3
Idifavour <idifavour7@gmail.com>
2022-06-01 11:06:37 +0400

    first commit

---
+++ products.js
@@ -0,0 +1,52 @@
+let products = [
+    {
+        "id": 1001,
+        "name": 'Men Jeans',
+        "description": ' Large sized Men Jeans for all occasions',
+        "price": 5000,
+        "image": "/images/product.png",
+        "stock": 5,
+        "rating": 5
+
+    },
+    {
+        "id": 1002,
+        "name": 'Men Jeans',
+        "description": ' Large sized Men Jeans for all occasions',
+        "price": 4000,
+        "image": "/images/product.png",
+        "stock": 9,
+        "rating": 3
+
+    },
+    {
+        "id": 1003,
+        "name": 'Men Jeans',
+        "description": ' Large sized Men Jeans for all occasions',
+        "price": 7000,
+        "image": "/images/product.png",
+        "stock": 11,
+        "rating": 4
+
+    },
+    {
+        "id": 1004,
+        "name": 'Men Jeans',
+        "description": ' Large sized Men Jeans for all occasions',
+        "price": 2000,
+        "image": "/images/product.png",
+        "stock": 8,
+        "rating": 3
+
+    },
+    {
+        "id": 1005,
+        "name": 'Men Jeans',
+        "description": ' Large sized Men Jeans for all occasions',
+        "price": 5000,
+        "image": "/images/product.png",
+        "stock": 10,
+        "rating": 5
+
+    },
+]
