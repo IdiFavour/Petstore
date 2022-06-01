f44166d9cdd0ec27a7114b32a4baaf431a7494b4
Idifavour <idifavour7@gmail.com>
2022-06-01 11:35:09 +0400

    first commit

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+var cacheName = 'petstore-v1'
+var cacheFiles = [
+    'index.html',
+    'products.js',
+    'petstore.webmanifest',
+    'images/product.png',
+    'images/icon-store-512.png',
+    'images/bg.jpg',
+    'css/todo.css'
+]
+
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] install')
+    e.waitUnitl(
+        caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching all the files')
+            return cache.addAll(cacheFiles)
+        })
+    )
+})
