61d87ceff51be3a6c25af6cbd65e40fcec16e9e1
Idifavour <idifavour7@gmail.com>
2022-06-01 11:41:08 +0400

    first commit

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@ var cacheFiles = [
 
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] install')
-    e.waitUnitl(
+    e.waitUntil(
         caches.open(cacheName).then((cache) => {
             console.log('[Service Worker] Caching all the files')
             return cache.addAll(cacheFiles)
